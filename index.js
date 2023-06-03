8bfcb839e868aad3216324b2748d82a986821ea7
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2023-06-03 14:16:04 +0530

    Set up MongoDB, update index.js

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
+const { json } = require('express');
+app.use(json());
+const mongoose = require('mongoose');
+mongoose.set("strictQuery", false);
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log('Connected to the Database!'))
+
+const userRouter = require('./routes/userRoutes');
+const postRouter = require('./routes/postRoutes');
+
+app.use("/api/auth", userRouter);
+app.use("/api/articles", postRouter);
+
 app.listen(PORT, () => {
-    console.log(`server listening on ${PORT}`);
+  console.log(`server listening on ${PORT}`);
 })
